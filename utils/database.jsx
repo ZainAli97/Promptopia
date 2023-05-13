561d65e45ded2194db80709470d2eee5cffbf810
Zain Ali <ZainSoftwear11@gmail.com>
2023-05-13 23:03:13 +0500

    Completed

--- utils/database.jsx
+++ utils/database.jsx
@@ -9,7 +9,7 @@ export const connectToDB = async () => {
     try {
         await mongoose.connect(
             process.env.MONGODB_URI, {
-            dbName: "share prompts",
+            dbName: "shareprompts",
             useNewUrlParser: true,
             useUnifiedTopology: true,
         }
