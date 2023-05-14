10d17493ad3781e9940e6c0a68c94b43ae3873ee
Zain Ali <ZainSoftwear11@gmail.com>
2023-05-14 23:15:10 +0500

    Created

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,8 +1,43 @@
-import React from 'react';
+"use client";
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import Form from '@components/Form';
 
 const CreatePrompt = () => {
+    const [submitting, setSubmitting] = useState(false);
+    const [post, setPost] = useState({
+        prompt: "",
+        tag: "",
+    });
+
+    const createPrompt = async (e) => {
+        e.preventDefault();
+        setSubmitting(true);
+        try {
+            const res = await fetch("/api/prompt/new", {
+                method: "post",
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    userId: session?.user.id,
+                    tag: post.tag,
+                })
+            });
+        } catch (error) {
+            console.log(error);
+        }
+        finally {
+
+        }
+    };
     return (
-        <div>Create Post</div>
+        <Form
+            type="Create"
+            post={post}
+            setPost={setPost}
+            submitting={submitting}
+            handleSubmit={createPrompt}
+        />
     );
 };
 
