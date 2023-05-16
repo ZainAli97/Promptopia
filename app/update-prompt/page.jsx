5884b5161b6b4168f93e7d661b07b55f176758ba
Zain Ali <ZainSoftwear11@gmail.com>
2023-05-16 23:05:25 +0500

    Completed

---
+++ app/update-prompt/page.jsx
@@ -0,0 +1,61 @@
+"use client";
+import { useState, useEffect } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import Form from '@components/Form';
+
+const EditPrompt = () => {
+    const searchParams = useSearchParams();
+    const promptId = searchParams.get("id");
+    const router = useRouter();
+    const [submitting, setSubmitting] = useState(false);
+    const [post, setPost] = useState({
+        prompt: "",
+        tag: "",
+    });
+
+    useEffect(() => {
+        const getPostDetails = async () => {
+            const response = await fetch(`/api/prompt/${promptId}`);
+            const data = response.json();
+            setPost({
+                prompt: data.prompt,
+                tag: data.tag
+            });
+        };
+        if (promptId) getPostDetails();
+    }, [promptId]);
+
+    const updatePrompt = async (e) => {
+        e.preventDefault();
+        setSubmitting(true);
+        if (!promptId) return alert("Prompt not found...");
+        try {
+            const res = await fetch(`/api/prompt/${promptId}`, {
+                method: "PATCH",
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    tag: post.tag,
+                })
+            });
+            if (res.ok) {
+                router.push("/");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        finally {
+            setSubmitting(false);
+        }
+    };
+    return (
+        <Form
+            type="Create"
+            post={post}
+            setPost={setPost}
+            submitting={submitting}
+            handleSubmit={updatePrompt}
+        />
+    );
+};
+
+export default EditPrompt;
