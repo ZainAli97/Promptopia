aa23bd8313de6804a07fe254a725a02266e96bce
Zain Ali <ZainSoftwear11@gmail.com>
2023-05-17 14:39:11 +0500

    Completed

--- models/prompt.js
+++ models/prompt.js
@@ -1,19 +1,20 @@
 import { Schema, model, models } from 'mongoose';
+
 const PromptSchema = new Schema({
-    creator: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-    },
-    prompt: {
-        type: String,
-        required: [true, "Prompt is required.."]
-    },
-    tag: {
-        type: String,
-        required: [true, "Tag is required.."]
-    }
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  prompt: {
+    type: String,
+    required: [true, 'Prompt is required.'],
+  },
+  tag: {
+    type: String,
+    required: [true, 'Tag is required.'],
+  }
 });
 
-//             either get the already existed prompt or create the new one. 
-const Prompt = models.Prompt || model("Prompt", PromptSchema);
+const Prompt = models.Prompt || model('Prompt', PromptSchema);
+
 export default Prompt;
--- models/user.js
+++ models/user.js
@@ -1,19 +1,21 @@
-import { Schema, model, models } from "mongoose";
+import { Schema, model, models } from 'mongoose';
+
 const UserSchema = new Schema({
-    email: {
-        type: "string",
-        unique: [true, "Email is already exists"],
-        required: [true, "Email is required"],
-    },
-    username: {
-        type: "string",
-        required: [true, "Username is required"],
-        match: [/^(?=.{8,20}$)(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain letters and can be unique."],
-    },
-    image: {
-        type: "string",
-    }
+  email: {
+    type: String,
+    unique: [true, 'Email already exists!'],
+    required: [true, 'Email is required!'],
+  },
+  username: {
+    type: String,
+    required: [true, 'Username is required!'],
+    match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+  },
+  image: {
+    type: String,
+  }
 });
 
 const User = models.User || model("User", UserSchema);
-export default User; 
+
+export default User;
